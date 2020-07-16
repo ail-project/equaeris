32428cfb76872f02c10b6b5f681545ca26e238b8
Trigleos <pthill99@gmail.com>
2020-07-16 10:54:22 -0400

    Test file for automated amazon s3 file extraction added to  tests

---
+++ tests/amazon_extraction.py
@@ -0,0 +1,15 @@
+import requests
+import xml.etree.ElementTree as ET
+
+
+def extract_bucket(bucketname):
+    url = "http://" + bucketname + ".s3.amazonaws.com/"
+    r = requests.get(url)
+    tree = ET.fromstring(r.text)
+    contents = tree[5]
+    for child in contents:
+        if "Key" in child.tag:
+            file = child.text
+            r1 = requests.get(url + file)
+            print(file , ":" , r1.text)
+extract_bucket("advent-bucket-one")
